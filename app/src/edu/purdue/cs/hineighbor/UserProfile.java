5c2b2f1516f8867ff68632ddc51c5c32993153d4
rmittal1105 <mittalr@purdue.edu>
2014-02-28 20:46:17 -0500

    Updated UserProfile.java
    Added connection with layout file.

--- app/src/edu/purdue/cs/hineighbor/UserProfile.java
+++ app/src/edu/purdue/cs/hineighbor/UserProfile.java
@@ -1,14 +1,22 @@
 package edu.purdue.cs.hineighbor;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserProfile extends Activity{
 
+	TextView name = (TextView) findViewById(R.id.tvName);
+	TextView birthday = (TextView) findViewById(R.id.tvBirthday);
+	TextView gender = (TextView) findViewById(R.id.tvGender);
+	TextView hobbies = (TextView) findViewById(R.id.tvHobbies);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_profile);
 	}
 
 }
