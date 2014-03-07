0d19fa47398674383e7f0ab04d96f29d8d91f5d1
“Edwin <wongyikfei@gmail.com>
2014-03-07 06:02:15 -0500

    Created a new class for holding the data in the listview

---
+++ app/src/edu/purdue/cs/hineighbor/Home_activity_list_class.java
@@ -0,0 +1,18 @@
+package edu.purdue.cs.hineighbor;
+
+public class Home_activity_list_class {
+	private String name;
+	private char[] hobbies;
+	/*
+	 * should the hobbies be an array of character or String?
+	 */
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	
+}
