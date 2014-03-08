f3a80064838a6a5af429699e5ecb2d85d07f32c4
“Edwin <wongyikfei@gmail.com>
2014-03-08 12:49:47 -0500

    modified

--- app/src/edu/purdue/cs/hineighbor/UserProfile.java
+++ app/src/edu/purdue/cs/hineighbor/UserProfile.java
@@ -11,14 +11,16 @@ public class UserProfile extends Activity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_profile);
 		//UI references
 		TextView name = (TextView) findViewById(R.id.nameOfUser);
 		TextView birthday = (TextView) findViewById(R.id.birthday);
 		TextView gender = (TextView) findViewById(R.id.gender);
 		TextView hobbies = (TextView) findViewById(R.id.hobbies);
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.user_profile);
 	}
 
 }
