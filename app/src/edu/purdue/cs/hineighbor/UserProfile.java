ea1b36a2463f302e2515945215b7911696c40a13
“Edwin <wongyikfei@gmail.com>
2014-03-08 12:48:42 -0500

    modified

--- app/src/edu/purdue/cs/hineighbor/UserProfile.java
+++ app/src/edu/purdue/cs/hineighbor/UserProfile.java
@@ -7,14 +7,15 @@ import android.widget.TextView;
 
 public class UserProfile extends Activity{
 
-	//UI references
-	TextView name = (TextView) findViewById(R.id.nameOfUser);
-	TextView birthday = (TextView) findViewById(R.id.birthday);
-	TextView gender = (TextView) findViewById(R.id.gender);
-	TextView hobbies = (TextView) findViewById(R.id.hobbies);
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//UI references
+		TextView name = (TextView) findViewById(R.id.nameOfUser);
+		TextView birthday = (TextView) findViewById(R.id.birthday);
+		TextView gender = (TextView) findViewById(R.id.gender);
+		TextView hobbies = (TextView) findViewById(R.id.hobbies);
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_profile);
